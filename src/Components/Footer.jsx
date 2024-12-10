26d0415cd8e42e198766f264353ddc706c5513d4
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-12-10 20:15:48 +0500

    [+] added components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,33 +5,32 @@ import UnderlinedLink from './UnderlinedLink'
 export default function Footer() {
   return (
     <>
-      <footer className='px-4 lg:px-32 py-8 flex flex-col justify-evenly gap-4'>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-6 items-center'>
+      <footer className='mt-6 px-4 lg:mx-40 py-8 flex flex-col justify-evenly gap-4 border-t'>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-6 first:items-start items-start'>
           <div className='flex flex-col gap-1 opacity-60'>
             <p>Sudais</p>
             <p>Web Developer</p>
             <p>Roll Num.</p>
           </div>
-          <div className='flex flex-col gap-1'>
-            <h4 className='opacity-60'>Links</h4>
+          <div className='flex flex-col gap-3'>
+            <h4 className='opacity-60 mb-4'>Links</h4>
             <Link href={"#"} className='link link-hover'>Home</Link>
             <Link href={"#"} className='link link-hover'>Shop</Link>
             <Link href={"#"} className='link link-hover'>About</Link>
             <Link href={"#"} className='link link-hover'>Contact</Link>
           </div>
-          <div className='flex flex-col gap-1'>
-            <h4 className='opacity-60'>Help</h4>
+          <div className='flex flex-col gap-3'>
+            <h4 className='opacity-60 mb-4'>Help</h4>
             <Link href={"#"} className='link link-hover'>Payment Options</Link>
             <Link href={"#"} className='link link-hover'>Returns</Link>
             <Link href={"#"} className='link link-hover'>Privacy Policy</Link>
             <Link href={"#"} className='link link-hover'>Contact</Link>
           </div>
-          <div className=''>
-            <h4 className='opacity-60'>Links</h4>
-            <div className='flex gap-3'>
-
-            <input type="email" placeholder='Enter your Email Address' className='input input-bordered underline underline-offset-8' />
-            <UnderlinedLink text={"SUBSCRIBE"} link={"#"} style={"text-sm"}/>
+          <div className='flex flex-col items-start h-full'>
+            <h4 className='opacity-60 mb-6'>Newsletter</h4>
+            <div className='flex gap-3 items-end'>
+              <input type="email" placeholder='Enter your Email Address' className='input input-bordered underline underline-offset-8' />
+              <UnderlinedLink text={"SUBSCRIBE"} link={"#"} style={"text-sm"} />
             </div>
           </div>
         </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,27 +2,31 @@ import { Search } from 'lucide-react'
 import { Heart } from 'lucide-react'
 import { ShoppingCart } from 'lucide-react'
 import { User } from 'lucide-react'
-import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
+import CartMenu from './CartMenu'
 
 export default function Header() {
   return (
     <>
       <section id='header-wrapper' className=''>
-        <header className='grid grid-cols-3 justify-center items-center'>
-          <div></div>
-          <nav className=''>
-            <Link className='btn btn-ghost' href={""}>Home</Link>
-            <Link className='btn btn-ghost' href={""}>Shop</Link>
-            <Link className='btn btn-ghost' href={""}>About</Link>
-            <Link className='btn btn-ghost' href={""}>Contact</Link>
+        <header className='p-4 lg:px-32 py-4 grid grid-cols-1 md:grid-cols-3 justify-center items-center'>
+          <div className='flex justify-center lg:justify-start'>
+            <Link href={"/"}>
+            <img className='w-12 aspect-square' src="/logo.png" alt="brand logo" />
+            </Link>
+          </div>
+          <nav className='flex justify-center items-center'>
+            <Link className='btn btn-ghost' href={"/"}>Home</Link>
+            <Link className='btn btn-ghost' href={"/shop"}>Shop</Link>
+            <Link className='btn btn-ghost' href={"/about"}>About</Link>
+            <Link className='btn btn-ghost' href={"/contact"}>Contact</Link>
           </nav>
-          <div className='flex gap-6'>
-            <User />
+          <div className='flex gap-6 justify-center md:justify-end'>
+            <Link href={"/account"}><User /></Link>
             <Search />
             <Heart />
-            <ShoppingCart />
+            <Link href={"/cart"}><CartMenu /></Link>
           </div>
         </header>
       </section>
