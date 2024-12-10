112558d5fb86e50a2eead0543397b2d2b47cc184
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-12-10 18:43:46 +0500

    [✨] styled homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import ContentWrapper from '@/Components/ContentWrapper'
 import Footer from '@/Components/Footer'
 import UnderlinedLink from '@/Components/UnderlinedLink'
 import { Calendar } from 'lucide-react'
@@ -8,61 +9,65 @@ import React from 'react'
 export default function Page() {
   return (
     <>
-      <section className='h-screen flex justify-evenly items-center bg-yellow-200' id="hero">
+      <section className='p-4 h-[680px] lg:h-screen flex flex-col-reverse lg:flex-row justify-center lg:justify-evenly items-center gap-4 bg-yellow-200' id="hero">
         <div className='flex flex-col gap-4'>
           <h2 className='text-4xl font-semibold'>Rocket Single Seater</h2>
           <Link className='link underline-offset-8' href={"#"}>Shop Now</Link>
         </div>
         <div className='flex flex-col'>
-          <img src="https://placehold.co/480x380" alt="image" />
+          <img className='w-full md:max-w-[680px] rounded-tl-full rounded-br-3xl' src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" />
         </div>
       </section>
       <section className='flex py-6 gap-3 justify-around' id="featured">
         <div className='flex flex-col gap-3'>
-          <img src="https://placehold.co/380" alt="image" />
+          <img className='max-w-[280px] aspect-square object-fill' src="/homepage/1.png" alt="image" />
           <h4 className='text-2xl font-medium'>Side Table</h4>
           <Link className='link underline-offset-8' href={"#"}>View More</Link>
         </div>
         <div className='flex flex-col gap-3'>
-          <img src="https://placehold.co/380" alt="image" />
+          <img className='max-w-[280px] aspect-square object-fill' src="/homepage/2.png" alt="image" />
           <h4 className='text-2xl font-medium'>Side Table</h4>
           <Link className='link underline-offset-8' href={"#"}>View More</Link>
         </div>
       </section>
 
-      <section id='top-picks' className=''>
+      <ContentWrapper id="top-picks">
         <h2 className='text-center text-4xl font-medium'>Top Picks for You</h2>
-        <p className='text-center opacity-60'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolore sint maxime autem itaque dolor!</p>
-        <div className='px-4 lg:px-32 py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-around'>
+        <p className='pb-10 text-center opacity-60'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolore sint maxime autem itaque dolor!</p>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-around'>
           {/* Top picks Item 1 */}
           <div className='flex flex-col items-start gap-2'>
-            <img className='block' src="https://placehold.co/240" alt="sofa image" />
+            <img className='block w-[280px] aspect-square object-contain' src="/homepage/3.png" alt="sofa image" />
             <h6>Trenton Modular Sofa 3</h6>
             <h4 className='text-xl font-semibold'>Rs. 25,000.00</h4>
           </div>
           {/* Top picks Item 2 */}
           <div className='flex flex-col items-start gap-2'>
-            <img className='block' src="https://placehold.co/240" alt="sofa image" />
+            <img className='block w-[280px] aspect-square object-contain' src="/homepage/4.png" alt="sofa image" />
             <h6>Trenton Modular Sofa 3</h6>
             <h4 className='text-xl font-semibold'>Rs. 25,000.00</h4>
           </div>
           {/* Top picks Item 3 */}
           <div className='flex flex-col items-start gap-2'>
-            <img className='block' src="https://placehold.co/240" alt="sofa image" />
+            <img className='block w-[280px] aspect-square object-contain' src="/homepage/5.png" alt="sofa image" />
             <h6>Trenton Modular Sofa 3</h6>
             <h4 className='text-xl font-semibold'>Rs. 25,000.00</h4>
           </div>
           {/* Top picks Item 4 */}
           <div className='flex flex-col items-start gap-2'>
-            <img className='block' src="https://placehold.co/240" alt="sofa image" />
+            <img className='block w-[280px] aspect-square object-contain' src="/homepage/6.png" alt="sofa image" />
             <h6>Trenton Modular Sofa 3</h6>
             <h4 className='text-xl font-semibold'>Rs. 25,000.00</h4>
           </div>
         </div>
-      </section>
+        <div className='flex justify-center mt-10'>
+        <UnderlinedLink text={"View More"}/>
+        </div>
+      </ContentWrapper>
+
 
       <section id='new-arrivals' className='px-4 lg:px-32 py-8 grid grid-cols-3'>
-        <img className='w-full object-cover col-span-2' src="https://placehold.co/280x180" alt="big sofa image" />
+        <img className='w-full object-cover col-span-2' src="/homepage/8.png" alt="big sofa image" />
         <div className='flex flex-col items-center justify-center gap-4'>
           <p className='font-medium'>New Arrivals</p>
           <h4 className='text-4xl font-bold'>Asgaard Sofa</h4>
@@ -74,33 +79,35 @@ export default function Page() {
         <h4 className='text-4xl font-bold'>Our Blogs</h4>
         <p className='text-center opacity-60'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolore sint maxime autem itaque dolor!</p>
         <div className='px-4 lg:px-32 py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-          <BlogPost />
-          <BlogPost />
-          <BlogPost />
+          <BlogPost imgUrl={"/blog/1.png"} />
+          <BlogPost imgUrl={"/blog/2.png"} />
+          <BlogPost imgUrl={"/blog/3.png"} />
         </div>
-        <UnderlinedLink text={"View More"}/>
+        <UnderlinedLink text={"View More"} />
       </section>
 
-      <section id='social' className='flex flex-col justify-center items-center relative overflow-hidden py-12'>
-        <img className='w-full absolute -z-10 opacity-30' src="https://picsum.photos/240" alt="" />
+      <section id='social' className='flex flex-col justify-center items-center relative overflow-hidden py-20'>
+        <img
+          className='w-full absolute -z-10 opacity-30'
+          src="https://images.unsplash.com/photo-1530762312300-888baa333d74?q=80&w=2068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
         <div className='flex flex-col gap-3'>
-        <h2 className='font-bold text-4xl'>Our Instagram</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <button className='btn rounded-full shadow-2xl btn-wide'>Follow Us</button>
+          <h2 className='font-bold text-4xl'>Our Instagram</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button className='btn rounded-full shadow-2xl btn-wide'>Follow Us</button>
         </div>
       </section>
-
-      <Footer />
     </>
   )
 }
 
-function BlogPost({ props }) {
+function BlogPost({ props, imgUrl }) {
   return (
     <>
       <div className='flex flex-col gap-3 items-center'>
-        <img className='rounded-md w-full' src="https://placehold.co/240" alt="" />
-        <p>Lorem ipsum dolor sit amet.</p>
+        <img className='rounded-md w-full' src={`${imgUrl ? imgUrl : "https://placehold.co/240}"}`} alt="" />
+        <p className='text-center'>Follow our store on Instagram.</p>
         <Link className='text-xl font-medium link underline-offset-8' href={"#"}>Read More</Link>
         <div className='py-3 flex gap-3'>
           <span className='flex gap-2 items-center text-sm'><Clock size={14} /><p>5 min</p></span>
